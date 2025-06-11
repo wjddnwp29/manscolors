2dd4a30a9a3f6e7a3b0a80c0ac11ab915f8af20c
wooje-jung <dnwpgmfk@naver.com>
2025-06-11 17:13:24 +0900

    2025 데이터베이스

---
+++ src/main/kotlin/kr/ac/kumoh/ai/s20231049/manscolors/ManscolorsApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.ai.s20231049.manscolors
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ManscolorsApplication
+
+fun main(args: Array<String>) {
+	runApplication<ManscolorsApplication>(*args)
+}
---
+++ src/test/kotlin/kr/ac/kumoh/ai/s20231049/manscolors/ManscolorsApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.ai.s20231049.manscolors
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ManscolorsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
